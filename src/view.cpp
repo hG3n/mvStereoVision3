9a25eb05b0e068876c73c27313075f41d0812345
hG3n <haghiller@gmail.com>
2015-08-19 19:13:59 +0200

    dynamically drawing of obstacle help grid

--- src/view.cpp
+++ src/view.cpp
@@ -2,61 +2,36 @@
 
 void View::drawObstacleGrid(cv::Mat &stream, int binning)
 {
-  if(binning == 0) 
-  {
-    //vertical lines
-    cv::line(stream, cv::Point(250,0), cv::Point(250,stream.rows), cv::Scalar(0,0,255), 1);
-    cv::line(stream, cv::Point(502,0), cv::Point(502,stream.rows), cv::Scalar(0,0,255), 1);
-    //horizontal lines
-    cv::line(stream, cv::Point(0,160), cv::Point(stream.cols, 160), cv::Scalar(0,0,255), 1);
-    cv::line(stream, cv::Point(0,320), cv::Point(stream.cols, 320), cv::Scalar(0,0,255), 1);
-  }
-  else
-  {
-    //vertical lines
-    cv::line(stream, cv::Point(250/2,0), cv::Point(250/2,stream.rows), cv::Scalar(0,0,255), 1);
-    cv::line(stream, cv::Point(502/2,0), cv::Point(502/2,stream.rows), cv::Scalar(0,0,255), 1);
-    //horizontal lines
-    cv::line(stream, cv::Point(0,160/2), cv::Point(stream.cols, 160/2), cv::Scalar(0,0,255), 1);
-    cv::line(stream, cv::Point(0,320/2), cv::Point(stream.cols, 320/2), cv::Scalar(0,0,255), 1);
-  }
+  int x = stream.cols/3;
+  int y = stream.rows/3;
+
+  //vertical lines
+  cv::line(stream, cv::Point(x,0),   cv::Point(x,stream.rows),   cv::Scalar(0,0,255), 1);
+  cv::line(stream, cv::Point(2*x,0), cv::Point(2*x,stream.rows), cv::Scalar(0,0,255), 1);
+
+  //horizontal lines
+  cv::line(stream, cv::Point(0,y),   cv::Point(stream.cols, y), cv::Scalar(0,0,255), 1);
+  cv::line(stream, cv::Point(0,y*2), cv::Point(stream.cols, y*2), cv::Scalar(0,0,255), 1);
 }
 
 void View::drawSubimageGrid(cv::Mat &stream, int binning)
 {
-  if (binning == 0)
-  {
-    // vertical lines
-    cv::line(stream, cv::Point(83,0), cv::Point(83,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(166,0), cv::Point(166,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(333,0), cv::Point(333,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(416,0), cv::Point(416,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(583,0), cv::Point(583,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(666,0), cv::Point(666,stream.rows), cv::Scalar(100,0,0), 1);
-    // horizontal lines
-    cv::line(stream, cv::Point(0,53), cv::Point(stream.cols,53), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,106), cv::Point(stream.cols,106), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,213), cv::Point(stream.cols,213), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,266), cv::Point(stream.cols,266), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,373), cv::Point(stream.cols,373), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,426), cv::Point(stream.cols,426), cv::Scalar(100,0,0), 1);
-  }
-  else
-  {
-    // vertical lines
-    cv::line(stream, cv::Point(83/2,0), cv::Point(83/2,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(166/2,0), cv::Point(166/2,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(333/2,0), cv::Point(333/2,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(416/2,0), cv::Point(416/2,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(583/2,0), cv::Point(583/2,stream.rows), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(666/2,0), cv::Point(666/2,stream.rows), cv::Scalar(100,0,0), 1);
-    // horizontal lines
-    cv::line(stream, cv::Point(0,53/2), cv::Point(stream.cols,53/2), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,106/2), cv::Point(stream.cols,106/2), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,213/2), cv::Point(stream.cols,213/2), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,266/2), cv::Point(stream.cols,266/2), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,373/2), cv::Point(stream.cols,373/2), cv::Scalar(100,0,0), 1);
-    cv::line(stream, cv::Point(0,426/2), cv::Point(stream.cols,426/2), cv::Scalar(100,0,0), 1);
-  }
+  int x = stream.cols/9;
+  int y = stream.rows/9;
+  
+  // vertical lines
+  cv::line(stream, cv::Point(x  ,0), cv::Point(x  ,stream.rows), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(x*2,0), cv::Point(x*2,stream.rows), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(x*4,0), cv::Point(x*4,stream.rows), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(x*5,0), cv::Point(x*5,stream.rows), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(x*7,0), cv::Point(x*7,stream.rows), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(x*8,0), cv::Point(x*8,stream.rows), cv::Scalar(100,0,0), 1);
 
+  // horizontal lines
+  cv::line(stream, cv::Point(0,y  ), cv::Point(stream.cols,y  ), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(0,y*2), cv::Point(stream.cols,y*2), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(0,y*4), cv::Point(stream.cols,y*4), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(0,y*5), cv::Point(stream.cols,y*5), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(0,y*7), cv::Point(stream.cols,y*7), cv::Scalar(100,0,0), 1);
+  cv::line(stream, cv::Point(0,y*8), cv::Point(stream.cols,y*8), cv::Scalar(100,0,0), 1);
 }
