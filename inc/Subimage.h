b1f2177e09acda8abf5c96a14d44f5612850b4e2
hG3n <haghiller@gmail.com>
2015-12-06 14:25:38 +0100

    added draw function

--- inc/Subimage.h
+++ inc/Subimage.h
@@ -13,6 +13,7 @@ struct Subimage
     roi_center(),
     value()
     {
+      // calculate the center point for later creation of pointcloud
       int temp_x = br.x - tl.x;
       int temp_y = br.y - tl.y;
       roi_center = cv::Point(tl.x + (temp_x/2), tl.y + (temp_y/2));
@@ -25,6 +26,11 @@ struct Subimage
     cv::Rect roi(tl,br);
     cv::Mat temp = dMap(roi);
     value = Utility::calcMeanDisparity(temp);
+  }
+
+  void draw(cv::Mat & dMap)
+  {
+    cv::rectangle(dMap, tl, br, cv::Scalar(255,0,0));
   }
 
   // member
