a6055722eb9cb046423eaca0f4be79b38b93c223
hG3n <haghiller@gmail.com>
2016-01-04 15:16:51 +0100

    new continuous capturing codes for either images or disparity maps

--- trgt/continousCapture.cpp
+++ trgt/continousCapture.cpp
@@ -1,167 +1,183 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+
+//system command
+#include <cstdlib>
 
 #include "Stereosystem.h"
 #include "disparity.h"
 #include "easylogging++.h"
+#include "view.h"
 
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 
+#include <ctime>
+
 INITIALIZE_EASYLOGGINGPP
+std::string tag = " Main ";
+bool running = true;
+
+int exposure = 10000;
+float gain = 4.0;
+bool hdr = false;
+
+bool capture = false;
+
+std::string pathLeft = "images/disparityMaps/left";
+std::string pathRight = "images/disparityMaps/right";
 
 int main(int argc, char* argv[])
 {
-	std::string tag = "MAIN\t";
+  std::string tag = "MAIN\t";
 
-	LOG(INFO) << tag << "Application started.";
-	mvIMPACT::acquire::DeviceManager devMgr;
+  LOG(INFO) << tag << "Application started." << std::endl;
+  mvIMPACT::acquire::DeviceManager devMgr;
 
-	Camera *left;
-	Camera *right;
+  Camera *left;
+  Camera *right;
 
-	if(!Utility::initCameras(devMgr,left,right))
-		return 0;
-
-
-	Stereosystem stereo(left,right);
-
-  std::vector<std::string> nodes;
-  nodes.push_back("capturedImages");
-
-  std::string config = "./configs/default.yml";
-
-  cv::FileStorage fs;
-
-
-  if(!Utility::checkConfig(config,nodes,fs))
-  {
+  // initialize cameras and create Stereosystem
+  if(!Utility::initCameras(devMgr,left,right))
     return 0;
+
+  Stereosystem stereo(left,right);
+
+  // reset intial exposure and request timeout in ms
+  left->setExposure(exposure);
+  right->setExposure(exposure);
+  left->setRequestTimeout(2000);
+  right->setRequestTimeout(2000);
+
+  // load settings from file and check for completeness
+  std::vector<std::string> nodes;
+  nodes.push_back("inputParameter");
+  std::string config = "./configs/default.yml";
+  cv::FileStorage fs;
+  if(!Utility::checkConfig(config,nodes,fs))
+    return 0;
+
+  std::string inputParameter;
+  fs["inputParameter"] >> inputParameter;
+
+  if(!stereo.loadIntrinsic(inputParameter+"/intrinsic.yml"))
+    return 0;
+  if(!stereo.loadExtrinisic(inputParameter +"/extrinsic.yml"))
+    return 0;
+
+  // create stereo image pair
+  Stereopair s;
+
+  char key = 0;
+  int binning = 0;
+
+  // get one single imagepair in order to receive the Q Matrix
+  // which is created during the rectification
+  stereo.getRectifiedImagepair(s);
+  Q = stereo.getQMatrix();
+  Q.convertTo(Q_32F,CV_32FC1);
+
+  // init OpenCV GUI objects
+  cv::namedWindow("Left", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("Right", cv::WINDOW_AUTOSIZE);
+
+  running = true;
+  int frame = 0;
+  while(running)
+  {
+    stereo.getRectifiedImagepair(s);
+    cv::imshow("Left", s.mLeft);
+    cv::imshow("Right", s.mRight);
+
+    if(capture)
+    {
+      std::string prefix = "";
+      if(frame < 10)
+      {
+        prefix +="000";
+      }
+      else if ((frame >=10) && (frame <100))
+      {
+        prefix += "00";
+      }
+      else if(frame >= 100)
+      {
+        prefix +="0";
+      }
+      cv::imwrite(std::string(pathLeft+"/left_"+prefix+std::to_string(frame)+".jpg"),s.mLeft);
+      cv::imwrite(std::string(pathRight+"/right_"+prefix+std::to_string(frame)+".jpg"),s.mRight);
+      ++frame;
+    }
+
+    key = cv::waitKey(2);
+    // keypress stuff
+    if(key > 0)
+    {
+      switch(key) {
+        case 'q':
+          LOG(INFO) << tag << "Exit requested" <<std::endl;
+          delete left;
+          left  = nullptr;
+          delete right;
+          right = nullptr;
+          cond_var.notify_one();
+          running = false;
+          break;
+        case 'b':
+        {
+          if (binning == 0)
+            binning = 1;
+          else
+            binning = 0;
+          left->setBinning(binning);
+          right->setBinning(binning);
+          stereo.resetRectification();
+          break;
+        }
+        case 'f':
+          std::cout<< left->getFramerate() << " " << right-> getFramerate() <<std::endl;
+          break;
+        case 'r':
+          reload = true;
+          break;
+        case 'E':
+          exposure += 1000;
+          left->setExposure(exposure);
+          right->setExposure(exposure);
+          break;
+        case 'e':
+          exposure -= 1000;
+          left->setExposure(exposure);
+          right->setExposure(exposure);
+          break;
+        case 'c':
+          if(!capture)
+            capture = true;
+          else
+            capture = false;
+          break;
+        case 'h':
+          if(!hdr){
+            left->enableHDR(Camera::hdr::ENABLE_HDR);
+            right->enableHDR(Camera::hdr::ENABLE_HDR);
+            hdr = true;
+            break;
+          } else {
+            left->enableHDR(Camera::hdr::DISABLE_HDR);
+            right->enableHDR(Camera::hdr::DISABLE_HDR);
+            hdr = false;
+            break;
+          }
+          break;
+        default:
+          std::cout << "Key pressed has no action" << std::endl;
+          break;
+      }
+    }
   }
 
-
-
-	Stereopair s;
-
-	left->setExposure(24000);
-	right->setExposure(24000);
-
-	std::string outputDirectory;
- 	fs["capturedImages"] >> outputDirectory;
-
-	std::string pathLeft = outputDirectory+"/left";
-  	std::string pathRight = outputDirectory+"/right";
-
-	unsigned int imageNumber = 0;
-
-
- 	if(Utility::directoryExist(pathLeft) || Utility::directoryExist(pathRight))
- 	{
- 		std::vector<std::string> tmp1, tmp2;
- 		Utility::getFiles(pathLeft,tmp1);
- 		Utility::getFiles(pathRight,tmp2);
-
- 		if(tmp1.size() != 0 || tmp2.size() != 0)
- 		{
- 			std::cout << "Output directory not empty, clean files? [y/n] " <<std::endl;
- 			char key = getchar();
-  			if(key == 'y')
-  			{
-  				std::system(std::string("rm " + pathLeft + "/* -f ").c_str());
-  				std::system(std::string("rm " + pathRight + "/* -f ").c_str());
-  			}
-  			else if(key == 'n')
-  			{
-  				imageNumber = tmp1.size();
-          		LOG(INFO) << tag << "Start with image number " << imageNumber << std::endl;
-  			}
- 		}
- 	}
-
-
-
-  if(Utility::createDirectory(pathLeft) && Utility::createDirectory(pathRight))
-    {
-            LOG(INFO) << tag << "Successfully created directories for captured images." << std::endl;
-    }
-    else
-    {
-        LOG(ERROR) << tag << "Unable to create directories for captured images." <<std::endl;
-        return 0;
-    }
-
-
-
-	char key = 0;
-	bool running = true;
-	bool capture = false;
-	unsigned int binning = 0;
-
-	cv::namedWindow("Left" ,1);
-	cv::namedWindow("Right" ,1);
-
-	while(running)
-	{
-
-		stereo.getImagepair(s);
-		cv::imshow("Left", s.mLeft);
-		cv::imshow("Right", s.mRight);
-
-		key = cv::waitKey(10);
-
-		if(capture)
-		{
-			std::string prefix = "";
-			if(imageNumber < 10)
-			{
-				prefix +="000";
-			}
-			else if ((imageNumber >=10) && (imageNumber <100))
-			{
-				prefix += "00";
-			}
-			else if(imageNumber >= 100)
-			{
-				prefix +="0";
-			}
-			cv::imwrite(std::string(pathLeft+"/left_"+prefix+std::to_string(imageNumber)+".jpg"),s.mLeft);
-			cv::imwrite(std::string(pathRight+"/right_"+prefix+std::to_string(imageNumber)+".jpg"),s.mRight);
-			++imageNumber;
-		}
-
-		if(key == 'q')
-		{
-			running = false;
-			break;
-		}
-		else if(key == 'b')
-		{
-		if (binning == 0)
-			binning = 1;
-		else
-			binning =0;
-
-		 left->setBinning(binning);
-		 right->setBinning(binning);
-		 stereo.resetRectification();
-		}
-		else if(key == 'f')
-		{
-		 std::cout << "FPS: Right "<< left->getFramerate() << " " << "Left " << right->getFramerate() << std::endl;
-		}
-		else if (key == 'c')
-		{
-			capture = !capture;
-			if(capture)
-			{
-				LOG(INFO) << tag << "Capture enabled" << std::endl;
-			}
-			else
-			{
-				LOG(INFO) << tag << "Capture disabled" << std::endl;
-			}
-		}
-	}
-	return 0;
+  // end the disparity thread
+  return 0;
 }
